2257566e33ea382237175784167c947852ebe2ec
Jay <jalentownsend08@yahoo.com>
2025-09-11 17:02:13 +0000

    ClassWork

---
+++ Unit 1/oprataors.py
@@ -0,0 +1,22 @@
+# Operators 
+# Operators are a construct (built-in system) that gives
+# data types more actions and power.
+
+# Arithmetic operator Family 
+# Arithmetic operators are simply math opreators.
+
+print(2 + 3)
+print(2 - 100)
+print(40 / 2)
+print(30.9 * 10)
+
+# Assigment opertor family 
+# these operators assign valus to variables
+# Otherwise known as containers
+# Key - value pairs
+
+schoolName = "Boys Latin High" 
+age = 16
+
+age += 1.5
+print(age)

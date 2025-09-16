07346953166a05c57764ea8b372e32d151b9db5c
Jay <jalentownsend08@yahoo.com>
2025-09-16 13:38:28 +0000

    ClassWork 16/9/25

---
+++ Unit 2/FuctionData.py
@@ -0,0 +1,32 @@
+# how to create a fuction with data
+
+# Step 1. Fuction definition
+def bnbRefund(accountNumber, refundAmount): 
+    print('account number: ' + accountNumber)
+    print('rfund amount: $' + refundAmount)
+
+#Step 2: Fuction call (invocation)
+bnbRefund(19292929, 3000) # arugments are real data
+
+def name_bday(name, bday)
+    print('My name is Jalen' + Jalen + ' My bady is ' + Jan 14)
+
+
+    name_bday('Jan', 'sep, 3rd')
+
+
+
+    # Create a fuction that will covert dollars into pennies 
+    # yoyr fuction shaould take the dollar amount in as a parmeter
+    # and calculate how many pennies their are in that dollar amount
+
+def dollarConverter(dollar);
+    pennies = dollar * 100
+    print('my '+ str(dollar) + 's is equal to ' + str(pennies) + ' Pennies.')
+
+    dollarConverter(12)
+
+    der dollarConverter(dollar):
+    pennies = dollar * 100
+    print
+

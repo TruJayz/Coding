12bdfc8a81f6b34be7e320595574972030e82920
Jay <jalentownsend08@yahoo.com>
2025-09-19 13:37:32 +0000

    Quiz 1

---
+++ Quiz.py
@@ -0,0 +1,58 @@
+# Create a new document called quiz1.py copy/ paste the following questions
+# on your document and answer the following questions.
+
+# quiz rules:
+# - There is no talking during the quiz
+# - You may only use your classnotes and w3schools.com for reference
+# - If you have a question about a quiz question, please raise your hand
+# - Once finished, submit your code to your repository using the source control 
+# button. Your commit should be "completed quiz 1."
+
+# 1. Name and describe three (3) built-in functions in python. 
+# Please write your responses in complete sentences.
+'Three built in functons are float(), help(), and Input() '
+
+# 2. What data type will the following snippet of code output?
+print(30/ 232)
+
+# 3.  Identify and name the operator family each of the following
+# symbols belongs to.
+
+# and 'Returns true if both statements are true'
+# == 'Equal'
+# >  'Greater than'
+
+# 4. Explain the difference between the = operator and the == operator.
+# Please write your response in complete sentences.
+'The == oprataor is equal than and = oprataor is Not equal than'
+
+# 5. Write code that takes a user’s input (as a string), 
+# casts it to a float, and prints the result multiplied by 2.
+print(float(2)* 8)
+
+# 6. What is the difference between a parameter and an argument?
+# Please write your response in complete sentences.
+'The difference between a parameter and a argument is that.'
+'A parameter tells you information can be passed to methods as parameter'
+'A arugmernt is information can be passed into functions'
+
+# 7. What is the difference between a function definition 
+# and a function invocation? # Please write your response in 
+# complete sentences.
+'The difference is that function invaoation'
+
+# 8. Why are functions useful in programming? Provide at least two reasons 
+# and write your reasons in in complete sentences.
+
+# 9. Write a code block that uses the appropriate operator for each scenario
+x = 15
+y = 20.
+# x is greater than y
+# x and 15 are both the same
+# x and y are not the same`
+
+# 10. Create a function that will take in two values. Your function should
+# take in 1 value as a parameter, and the other value should be passed in by the
+# user through the terminal. Your function should compare if the number passed in by
+# the user is greater than the number passed in via the parameter and should print out
+# the appropriat response of true or false.

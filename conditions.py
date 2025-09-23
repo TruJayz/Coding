f1b171ff0072d2f65a504ff63b9af7b55b468334
Jay <jalentownsend08@yahoo.com>
2025-09-23 13:33:10 +0000

    ClassWork 9/23/25

---
+++ conditions.py
@@ -0,0 +1,27 @@
+# Conditional statements = code that has a set of different 
+# outcomes based on the date that is given.
+
+#  condidtional syntax = if / else 
+weather = input("whats the weather like today ?")
+
+if weather == 'sunny':
+    print("Its gonna be nice out, wear shades.")
+elif
+    print(weather == 'rainy')
+    print("remember to bring to an umbralla")
+elif weather == 'chilly':
+    print('bring a jacket')
+else:
+    print("have a good day.")
+
+
+passwrd = input
+
+ if password =='2004':
+    print('welcome! you are logged in.')
+ else:
+    print('incorrect password')
+
+
+
+

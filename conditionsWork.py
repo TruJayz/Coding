0128e6ad2a2b8d6dea34cbf0cb7bca4ff662d685
Jay <jalentownsend08@yahoo.com>
2025-09-30 13:37:22 +0000

    Classwork 9/29/25

---
+++ conditionsWork.py
@@ -0,0 +1,31 @@
+
+def Agechecker(age):
+  if age  >=18:
+   print("You are able to vote")
+else:
+print("sorry, your not old enough yet")
+
+Agechecker(18)
+
+
+# Question 2
+
+def maxnumber():
+  a = int(input ("Please enter your first number: "))
+   b = int(input ("Please enter your second number: "))
+   c = int(input ("Please enter your third number: "))
+  HighNumber = max(a, b, c)
+ print(HighNumber)
+
+  MaxNumber()
+
+# Question 3
+
+def MovieTicket():
+  age = int(input("How old are you? "))
+  if age <=13:
+    print('your ticket is $10.00')
+    elif age >=25
+print('your ticket is $20.00')
+else:
+print('Your ticket is $10.00')

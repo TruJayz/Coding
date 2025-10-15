5ac887c5ea7e680085c3ba00ee3f0abe7613bdc1
Jay <jalentownsend08@yahoo.com>
2025-10-15 13:37:32 +0000

    ClassWork 10/15/25

---
+++ Unit 3/ListsProgram3.py
@@ -0,0 +1,39 @@
+
+
+Foodlist = ["Pasta" , "waffles", "pizza rolls", "cake"]
+
+print(Foodlist[0]) # first item on the list
+print(Foodlist[4]) # last item on the list
+
+food.pop(2)
+print(Foodlist)
+food,insert(2, 'pizza rolls')
+print(Foodlist) # Adds the item at a specified location.
+
+
+Breakfast = ['Pancakes','waffles','cereal','breakfast sandwitch']
+
+def foodMenu(list):
+Breakfast = input("please seclect a breakfast item")
+list.append(newitem)
+foodMenu()
+
+print("Hello what time of day is it?")
+x = input()
+print("")
+
+
+
+def MenuBytime():
+    print("Morining, afternoon, night")
+    time = input(" What time of day is it? : ")
+
+if time == 'morining':
+    print('Breakfaat time')
+    print(breakfast)
+elif time == 'afternoon':
+    print('Lunch time')
+    print(lunch)
+elif time == 'night':
+
+    MenuBytime

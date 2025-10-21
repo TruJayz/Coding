927aa2b42a3acac24a3d68554bbe86e4effd3b34
Jay <jalentownsend08@yahoo.com>
2025-10-21 13:37:37 +0000

    Classwork 21/10/25

---
+++ Whileloops.py
@@ -0,0 +1,27 @@
+number = 0 
+while number < 10:
+    number += 1
+    print(number)
+else: 
+    print('Done Counting')
+
+
+    # create a while loop that counts down from 30 by 1.
+
+
+    i = 1
+    while i < 30:
+        print(i)
+        i += 1 
+
+
+timer = 30
+while number < 0:
+
+timer -=1
+print(timer)
+else: 
+
+# if its false, we get this
+print('done count down, ')
+ 

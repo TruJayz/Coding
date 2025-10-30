79a32d5c77db3be4d02729f8f5cc053b7fb97e3c
Jay <jalentownsend08@yahoo.com>
2025-10-30 13:37:24 +0000

    Classwork 30/10

---
+++ Unit 3/WhileloopAcivity.py
@@ -0,0 +1,96 @@
+y = True
+while y == True:
+#  x = input("Enter a number:")
+  try:
+    x = float(x);
+  #  y = False
+  except:
+ #   print("Wrong input, please try again.")
+
+i = 0
+#while i < 40:
+  #print(i)
+  i += 1 
+
+
+
+
+  def numberloop(): 
+
+      while numberinput  !='quit':
+       numberinput = input('please enter a number: ')
+      numberlist = []
+      numberlist.append(numberinput)
+      print(numberlist)
+     numberinput = input('plese enter a number')
+    print('code is working')
+
+    else:
+          print('loop has ended, ')
+          print('code is working able to quit')
+
+numberloop()
+
+
+
+
+
+
+
+i = 1
+print(i)
+print('beginning of story')
+x= input('type something')
+print('middle of story')
+y= input('type something else')
+print('end of story')
+z= input('write 1 more thing')
+i += 1
+print(i)
+
+
+
+
+
+def numberlistloop():
+ numbertlist = []
+ usernumber = input('type a number')
+ while usernumber != 'quit':
+    newNumber int(usernumber)
+    numberlist.append(newNumber)
+    print(numbertlist)
+    usernumber = input('type in a number: ')
+ else:
+    print("loop has endded")
+
+    numberlistloop()
+
+
+
+    # def Numberlist():
+       
+
+       # Acivity 3
+
+       def randomNumber():
+          correctNumber = 7
+          userGuess = input('plese guess a number')
+          while userGuess != correctNumber:
+             userGuess = int(input('please guess a number: '))
+             if userGuess > correctNumber
+             print('your guess is to higth')
+
+          else:
+             print('your guess is to low')
+             userGuess = int(input("please guess a number: "))
+
+          else:
+          print("congrats, you got the correct number! ")
+
+
+            randomNumber()
+         
+
+
+
+

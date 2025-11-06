e47e420d63561933ed447f81c0c3ac243ff7bf91
Jay <jalentownsend08@yahoo.com>
2025-11-06 14:37:46 +0000

    classwork 6/11/25

---
+++ forloops.py
@@ -0,0 +1,46 @@
+for x in range(5):
+word = input ('please type a word: ')
+print(word)
+
+
+def tf():
+for x in range(5):
+print("tree or false: 3 is greater than 2")
+
+
+# Use a for loop to ask a user to type in 5 words and print each word out in
+# the terminal. Once the user has finished typing 5 words, 
+# the for loop should end. 
+
+# Clarification: program should ask the user to type in one word. Then the program
+# should print it out and ask them to type another word. Your program
+# should do this 5 times
+
+
+
+# looping through strings 
+word = 'p y t h o n'
+for  letter = word
+    print(letter)
+if letter == "P":
+    print(' did u mean paper')
+    else letter =="y"
+print("did u mean: python")
+
+
+
+
+
+
+
+
+# looping through lists of numbers
+shopping = [3.00, 5.40, 7.20, 9.00]
+total = 0
+tax = 0.7
+discount = 10.00
+newtotal = 0
+for items in shopping:
+    total += items
+
+    print(total)

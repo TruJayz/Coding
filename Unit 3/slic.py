09c3564923cf98153055a482e0d0e1ceb67f4670
Jay <jalentownsend08@yahoo.com>
2025-11-14 14:37:22 +0000

    Sdlc

---
+++ Unit 3/slic.py
@@ -0,0 +1,53 @@
+def sighup():
+    dob = int(input('what year you were born? '))
+    tiktok_kids = [] # 8 -12 is kids
+    tiktok_teens = [] # 13 -18 is teens
+    tiktok_adults = [] # 19 > is adults
+    currntYear = 2025
+    userAge = currntYear - dob
+    if userAge > 8 and userAge < 12:
+        print('welcome to tiktok kids')
+        tiktok_kids.append(userAge)
+    elif userAge > 12 and userAge < 18:
+        print('welcome to tiktok teens')
+        tiktok_teens.append(userAge)
+    else:
+        print('welcome to tiktok')
+        tiktok_adults.append(userAge)
+
+
+sighup()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Step 1
+
+# Some features that my calculator app should have is.
+
+'Is simple subtraction'
+'mulitplcation'
+'addition'
+'and combining like terms'
+
+
+# Step 2
+
+'For the addition it should be able to add to numbers together'
+
+
+def math():
+        a = input('enter a number:')
+        b = 10
+        print(a + b)

df33783d7a4b80cb6d08a42e08ba3f0cafa91979
Jay <jalentownsend08@yahoo.com>
2025-12-03 14:37:29 +0000

    Classwork 12/3/25

---
+++ Projects/MiniProject1.py
@@ -0,0 +1,36 @@
+
+
+
+
+def Quiz():
+
+    print("Question 1")
+    print("Who is the best character in Bluelock")
+    selection = input()
+    if selection == 'a':
+     print("Nagi")
+    if selection == 'b':
+     print("Rin itoshi")
+    if selection == 'c':
+      print("Sae itoshi")
+    if selection == 'd':
+       print("Isagi")
+    else:
+      print("Question 2")
+    print("Who is the best character in Dragon ball super")
+    if selection == 'a':
+      print("Gohan")
+      if selection == 'b':
+        print('Goku')
+        if selection == 'c':
+          print("gogeta")
+          if selection == 'd':
+           print("Hit")
+        else:
+            print('question 3')
+
+
+
+
+            Quiz()
+

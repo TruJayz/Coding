d9f5bd714e47a43fbe8dcc1eaa47dc00745f967f
Jay <jalentownsend08@yahoo.com>
2025-12-05 14:37:15 +0000

    work

--- Projects/MiniProjectElse.py
+++ Projects/MiniProjectElse.py
@@ -6,11 +6,39 @@ def pythonQuiz():
     print("A. Fuction")
     print("B. Integer")
     print("C. Float")
-    print("String")
-    userAnwser = input('')
+    print("D. String")
+    userAnwser = input()
     CorrectAnwser = 'a'
     if userAnwser == CorrectAnwser:
         grade +=1
         print('correct')
     else:
         print('Incorrect')
+    print('2. What of the fowllowing is a string?')
+    print("A. # ")
+    print("B."" ")
+    print("C. == ")
+    print("D. + ")
+    userAnwser = input()
+    CorrectAnwser = 'B'
+    if userAnwser == CorrectAnwser:
+        grade +=1
+        print('correct')
+    else:
+        print('incorrect')
+        print("3. What of the following is a lists")
+        print("A. () ")
+        print("B. { } ")
+        print("C. [] ")
+        print("D. ++ ")
+        userAnwser = input()
+        CorrectAnwser = 'C'
+        if userAnwser == CorrectAnwser:
+            grade +=1
+            print('correct')
+        else:
+            print('incorrect')
+
+            print('Here is youur score: ' + str(grade) + ' /3')
+
+            pythonQuiz()
